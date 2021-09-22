4b45c2ddbf2cca8326a896bd958e343c341367c0
iman9922 <nano20021984@gmail.com>
2021-09-22 03:26:28 +0300

    this is finall updated till now

--- firstsec.js
+++ firstsec.js
@@ -40,24 +40,24 @@ async function renderAffectedCountries() {
     <h4>Total cases</h4>
     <p><span class=" span1 ">${ totalcases.toLocaleString()}</span><span class="span2 ">+ ${ todayCases.toLocaleString()} </span></p>
     <p><span class="span3 ">${casesPerOneMillion.toLocaleString()} per million .</span>
-        <span class="span4 ">165,2558 tested</span></p>
+        </p>
 </div>
 <div class="activeCases ">
     <i class="fas fa-head-side-mask " style="color: #3b81f7; "></i>
     <h4>Active cases</h4>
-    <p><span class="span1 ">${active.toLocaleString()}</span><span class="span5 "> -13,854 </span></p>
+    <p><span class="span1 ">${active.toLocaleString()}</span></p>
     <p><span class="span3 ">${critical.toLocaleString()} in critical conditions</span></p>
 </div>
 <div class="recovered ">
     <i class="fas fa-heart " style="color: green; "></i>
     <h4>Recovered</h4>
-    <p><span class="span1 ">${recovered.toLocaleString()} </span><span style="color: green; ">+117,080</span>
+    <p><span class="span1 ">${recovered.toLocaleString()} </span>
         <br><br><span class="span5 ">   ${parseInt(100*recovered/totalcases)}% recovered </span></p>
 </div>
 <div class="deaths ">
     <i class="fas fa-skull-crossbones " style="color:#ff1e4a ; "></i>
     <h4>Deaths</h4>
-    <p><span class="span1 ">${deaths.toLocaleString()}</span><span class="span2 "> +545,985 </span></p>
+    <p><span class="span1 ">${deaths.toLocaleString()}</span></p>
     <p><span class="span3 ">${deathsPerOneMillion.toLocaleString()} Per million.</span></p>
 </div>`)
 
